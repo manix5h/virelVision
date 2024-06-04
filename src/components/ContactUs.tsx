ff5eacf14f434337980773b8c860e5b913dbe7ed
manishsinghmodi <manishsahu43962@gmail.com>
2024-06-04 11:03:59 +0530

    add dist files

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -43,7 +43,7 @@ export const ContactUs = () => {
     }
   };
   return (
-    <form  className="bg-white dark:bg-gray-900" id="contactus">
+    <form  className="bg-white dark:bg-gray-900 pl-5 md:pl-1  justify-center" id="contactus">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Contact Us
@@ -68,7 +68,7 @@ export const ContactUs = () => {
               type="email"
               id="email"
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="Prateek Jain"
               required
             />
           </div>
